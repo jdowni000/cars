f35cf3e5a34bbd19bb874c09058d73ae3a5f682e
Justin Downie <jusitn_downie@cable.comcast.com>
2020-01-31 10:43:09 -0800

    added version 3 Supra

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 build:
-	docker build -f cmd/car-server/Dockerfile -t jdowni000/temp:v2 .
+	docker build -f cmd/car-server/Dockerfile -t jdowni000/temp:v3 .
 
 push:
-	docker push jdowni000/temp:v2
+	docker push jdowni000/temp:v3
--- cmd/car-server/main.go
+++ cmd/car-server/main.go
@@ -24,8 +24,8 @@ func handleCarRequest(resp http.ResponseWriter, req *http.Request) {
 	m["Chevrolet"] = carcreator.NewCarWithOutput("Corvette", "red", resp)
 	m["Dodge"] = carcreator.NewCarWithOutput("Viper", "Silver", resp)
 
-	if "Roadster" != userCar {
-		resp.Write([]byte("Dude pick a real car like a Roadster"))
+	if "Supra" != userCar {
+		resp.Write([]byte("Dude pick a real car like a Supra"))
 		return
 	}
 
